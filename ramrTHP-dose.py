9fee99748c13bab1dc930f2424f858b7cb5dbb09
simonsnitz <simonsnitz@gmail.com>
2019-12-17 12:17:57 -0600

    abstracted Ec50 plotting program more. fitted sigmoids and individual data points included

--- ramrTHP-dose.py
+++ ramrTHP-dose.py
@@ -14,72 +14,75 @@ fig,ax = plt.subplots()
 dfe = pd.DataFrame()
 data = pd.read_csv("testData.csv")
 
-#trash code for generating labels. Labels array is hard coded :/
-labels = ['WT','NOS1','NOS2','NOS3','NOS4']
+
+#calculate the background
+bkgrd = data.loc[:,"DH10B"].mean()
+
+
+#Dannys more readable, more robust, faster (uses C++) code
+#def rm_background(col, background, skip=['Noscapine', 'DH10B']):
+#    if col in skip:
+#        return col
+#    return col - background
+#
+#new_df = data.apply(lambda x: rm_background(x,bkgrd))
+
+
+#update dataframe by subtracting background from each value           Assumes DH10B column is last column.
+for i in range(1,len(data.iloc[0]) -1):
+    col = data.iloc[:,i]
+    for n in range(0,len(col)):
+        data.iloc[:,i][n] = col[n]-bkgrd
+
+#Generate variant name labels from column labels. Loop array values are hard-coded, assume we have WT and 4 mutants to compare. Can change later...
+labels = []
+for i in [1,4,7,10,13]:
+    labels.append(data.columns.values[i][:-2])
+
+#Generate 'average' variant labels.
 avgLabels = list(range(0,len(labels)))
 for i in range(0,len(labels)):
     avgLabels[i] = labels[i]+'avg'
 
-#Add new columns for variants fluorescence averages. Numbers 5 and 12 are hard coded here :/
+#Add new columns for variant's fluorescence averages. Numbers 5 and 12 are hard coded here, assumes you have WT and 4 mutants, 12 concentrations.
 numCon = list(range(0,12))
 for i in range(0,5):
     data[avgLabels[i]] = numCon
 
-#print(data.loc[0:,'WT1'])
-#print(data.iloc[0:,0])
-
-#Fill in data for average values. 1st identifiers can be abstracted. 2nd will be a bit harder.
+#create an array of labels used to generate mean. For example ['WT-1','WT-3', ...]
+meanLabels = []
+for i in [1,3,4,6,7,9,10,12,13,15]:
+    meanLabels.append(data.columns.values[i])
+#Fill in data for average values. Abstracted! Assumes you have 3 data points you're averaging.
 for i in numCon:
-    data.loc[i,'WTavg'] = data.loc[i, 'WT1':'WT3'].mean()
-    data.loc[i,'NOS1avg'] = data.loc[i, 'NOS1-1':'NOS1-3'].mean()
-    data.loc[i,'NOS2avg'] = data.loc[i, 'NOS2-1':'NOS2-3'].mean()
-    data.loc[i,'NOS3avg'] = data.loc[i, 'NOS3-1':'NOS3-3'].mean()
-    data.loc[i,'NOS4avg'] = data.loc[i, 'NOS4-1':'NOS4-3'].mean()
-
-
-xdata = data.iloc[:,0]
-#loop through this part
-#for i in ___:
-ydata = data['NOS3avg']
-
-'''
-#example data
-xdata = np.array([0.0, 1.0, 10.0, 100.0, 1000.0])
-ydata = np.array([1300, 1300.0, 1691.0, 10479.8, 59064.1])
-ydataerr = [120.8, 120.8, 140.0, 160.5, 1181.0]
-#set error bars
-plt.errorbar(xdata, ydata, yerr=ydataerr, fmt='bo', capsize=5, label="data")
-'''
+    counter = 0
+    for n in range(0,len(avgLabels)):
+        data.loc[i,avgLabels[n]] = data.loc[i,meanLabels[counter]:meanLabels[(counter+1)]].mean()
+        counter += 2
 
 #define sigmoid function
 def sigmoid(x, a, b, c, d):
     return ((a-b) / (1.0 + np.exp(x - (c)) ** d)) +b
 
-
-#Run curve_fit function
-#both initial values work. Doesn't change anything
-#popt, pcov = curve_fit(sigmoid, xdata, ydata, p0 = [1.0,1.0,1.0,0.5])
-popt, pcov = curve_fit(sigmoid, xdata, ydata, p0 = [1.0, 70000.0, 1.0, 1.0])
 x = np.linspace(0,20000,100000)
-print(pcov)
+xdata = data.iloc[:,0]
+colorLines = ['b-','g-','r-','c-','y-']
+#Loop through mutants and plot fitted sigmoid functions
+counter = 0
+for i in avgLabels:
+    popt, pcov = curve_fit(sigmoid, xdata, data[i], p0 = [1.0, 65000.0, 1.0, 0.03])
+    plt.plot(x, sigmoid(x, *popt), colorLines[counter], label='fit')
+    counter += 1
 
-#Plot fitted sigmoid function
-plt.plot(x, sigmoid(x, *popt), 'r-', label='fit')
+
+#print(data.columns.values)
+lig = data.iloc[:,0]
+colorDots = ['bo','bo','bo','go','go','go','ro','ro','ro','co','co','co','yo','yo','yo']
+for i in range(1,16):
+    variant = data.columns.values[i]
+    plt.plot(lig, data.loc[:,variant],colorDots[i-1])
 
 '''
-popt, pcov = curve_fit(sigmoid, xdata, ydata, p0 = [1.0, 65000.0, 1.0, 0.03])
-plt.plot(x, sigmoid(x, *popt), 'g-', label='fit')
-ydata = data['NOS2avg']
-popt, pcov = curve_fit(sigmoid, xdata, ydata, p0 = [1.0, 65000.0, 1.0, 0.03])
-plt.plot(x, sigmoid(x, *popt), 'b-', label='fit')
-ydata = data['NOS3avg']
-popt, pcov = curve_fit(sigmoid, xdata, ydata, p0 = [1.0, 65000.0, 1.0, 0.03])
-plt.plot(x, sigmoid(x, *popt), 'y-', label='fit')
-ydata = data['NOS4avg']
-popt, pcov = curve_fit(sigmoid, xdata, ydata, p0 = [1.0, 65000.0, 1.0, 0.03])
-plt.plot(x, sigmoid(x, *popt), 'c-', label='fit')
-'''
-
 #plot individual data points
     #set x axis
 lig = data.iloc[:,0]
@@ -87,13 +90,15 @@ lig = data.iloc[:,0]
 plt.plot(lig, data.loc[:,'NOS3-1'], 'bo')
 plt.plot(lig, data.loc[:,'NOS3-2'], 'bo')
 plt.plot(lig, data.loc[:,'NOS3-3'], 'bo')
+'''
+#extract compound name from data frame
+compound = str(data.columns.values[0])
 
 plt.xlim(0.05,200)
 plt.ylim(-1000,80000)
 plt.ylabel('Fluorescence (RFU/OD600)', fontsize=20)
-#pull compound name from data frame
-plt.xlabel('Noscapine (uM)', fontsize=20)
-plt.title('RamR response to Noscapine', fontsize=25)
+plt.xlabel(compound+' (uM)', fontsize=20)
+plt.title('RamR response to '+compound, fontsize=25)
 ax.set_xscale('log')
 ax.tick_params(axis='both', which='major', labelsize='18')
 
